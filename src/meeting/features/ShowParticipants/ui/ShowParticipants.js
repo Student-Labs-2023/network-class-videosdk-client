4802a5664c4a2359ce1a884e27750576d4174d7b
unknown <emilnovikov392@gmail.com>
2023-08-14 02:23:39 +0600

    add showing participants

--- src/meeting/features/ShowParticipants/ui/ShowParticipants.js
+++ src/meeting/features/ShowParticipants/ui/ShowParticipants.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import { sideBarModes } from '../../../../utils/common';
+import { useMeetingAppContext } from '../../../../MeetingAppContextDef';
 
 const Button = styled.button`
     position: absolute;
     top: 21px;
     right: 0; 
     z-index: 100;
-    
+
     display: flex;
     width: 48px;
     height: 48px;
@@ -20,8 +22,16 @@ const Button = styled.button`
 `
 
 export const ShowParticipants = () => {
+    const { sideBarMode, setSideBarMode } = useMeetingAppContext();
+
   return (
-    <Button>
+    <Button         
+        onClick={() => {
+            setSideBarMode((s) =>
+                s === sideBarModes.PARTICIPANTS ? null : sideBarModes.PARTICIPANTS
+            );
+        }}
+    >
         <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
             <path d="M11 18C13.2091 18 15 16.2091 15 14C15 11.7909 13.2091 10 11 10C8.79086 10 7 11.7909 7 14C7 16.2091 8.79086 18 11 18Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M6 23C6 20.2 8.2 18 11 18C13.8 18 16 20.2 16 23" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
